88f33d592c395ad4be5c4fe59c79de2edfb60fad
Soham Nayak <sohamnayak04@gmail.com>
2025-06-14 10:17:31 +0530

    5th

--- src/app/api/codechef/[username]/route.ts
+++ src/app/api/codechef/[username]/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(
-  request: NextRequest,
+  request: Request,
   { params }: { params: { username: string } }
 ) {
   try {
